e92db8f17949ac8b434bf794a0f7a9d0542baf79
ywwg <ywwg@a0c6dda6-8710-0410-9408-9ac0bdba4c84>
2006-11-05 00:40:55 +0000

    git-svn-id: https://svn.code.sf.net/p/penguintv/code/trunk@180 a0c6dda6-8710-0410-9408-9ac0bdba4c84

---
+++ PenguinTVActivity.py
@@ -0,0 +1,31 @@
+import sys, os, logging
+import gtk, gobject
+from sugar.activity.Activity import Activity
+
+#need to set things up before we import penguintv
+
+try:
+	import pycurl
+except:
+	logging.warning("Trying to load bundled pycurl libraries")
+	
+	import PenguinTVActivity
+	activity_root = os.path.split(PenguinTVActivity.__file__)[0]
+	os.environ['LD_LIBRARY_PATH'] += ':'+os.path.join(activity_root, 'lib')
+	os.environ['PYTHONPATH'] += ':'+os.path.join(activity_root, 'site-packages')
+	import pycurl #if it fails now, let it fail
+os.environ['SUGAR_PENGUINTV'] = '1' #set up variable so that utils knows we are running_sugar
+
+from penguintv import penguintv
+
+class PenguinTVActivity(Activity):
+	def __init__(self):
+		Activity.__init__(self)
+		app = penguintv.PenguinTVApp()
+		app.main_window.Show(self)
+		app.post_show_init()
+		self.connect('destroy',self.do_quit, app)
+	
+	def do_quit(self, event, app):
+		app.do_quit()
+		del app
