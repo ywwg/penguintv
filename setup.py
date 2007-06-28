9ecf7c63329879bf662ebfb8808a6043f582e5c9
ywwg <ywwg@a0c6dda6-8710-0410-9408-9ac0bdba4c84>
2007-06-28 03:30:17 +0000

    * (olpc) fix for bundlebuilder api change
    git-svn-id: https://svn.code.sf.net/p/penguintv/code/trunk@348 a0c6dda6-8710-0410-9408-9ac0bdba4c84

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if HAS_SUGAR:
 			print "There was an error symlinking the glade file"
 			sys.exit(1)
 
-		bundlebuilder.start('MANIFEST-OLPC')
+		bundlebuilder.start(manifest='MANIFEST-OLPC')
 	except Exception, e:
 		print "problem building for OLPC:", e
 		sys.exit(1)
