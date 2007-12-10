0be486c0a0dd2c3e5ba07fa62ea593b775952570
ywwg <ywwg@a0c6dda6-8710-0410-9408-9ac0bdba4c84>
2007-12-10 03:28:07 +0000

    * (hildon) hildon listener doesn't do anything for now
    git-svn-id: https://svn.code.sf.net/p/penguintv/code/trunk@431 a0c6dda6-8710-0410-9408-9ac0bdba4c84

--- penguintv/HildonListener.py
+++ penguintv/HildonListener.py
@@ -17,18 +17,3 @@ class HildonListener:
 	def __init__(self, app, h_window):
 		self._app = app
 		self._h_window = h_window
-		
-		self._h_window.connect('key_press_event', self.on_hildon_key_press_event)
-		
-	def on_hildon_key_press_event(self, widget, event):
-		
-	
-	
-		keyname = gtk.gdk.keyval_name(event.keyval)
-		
-		logging.debug("key: %s" % keyname)
-		
-		if keyname == 'KP_Left' or keyname == 'Left':
-			self._app.feed_list_view.grab_focus()
-		elif keyname == 'KP_Right' or keyname == 'Right':
-			self._app.entry_view.grab_focus()
