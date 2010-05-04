f889db4552f3bdb6c86f78652012dc510af1ed44
ywwg <ywwg@a0c6dda6-8710-0410-9408-9ac0bdba4c84>
2010-05-04 03:33:19 +0000

    * bugfix for incorrect db object call
    git-svn-id: https://svn.code.sf.net/p/penguintv/code/trunk@858 a0c6dda6-8710-0410-9408-9ac0bdba4c84

--- penguintv/penguintv.py
+++ penguintv/penguintv.py
@@ -2505,7 +2505,7 @@ class PenguinTVApp(gobject.GObject):
 					if update_data.has_key('first_poll'):
 						if update_data['first_poll']:
 							self._gui_updater.queue(self._mark_all_media_but_first, feed_id)
-					if db.get_flags_for_feed(feed_id) & ptvDB.FF_DOWNLOADSINGLE == ptvDB.FF_DOWNLOADSINGLE:
+					if self.db.get_flags_for_feed(feed_id) & ptvDB.FF_DOWNLOADSINGLE == ptvDB.FF_DOWNLOADSINGLE:
 						self._gui_updater.queue(self._mark_all_media_but_first, feed_id)
 			elif not cancelled and feed_id != -1:
 				#check image just in case
